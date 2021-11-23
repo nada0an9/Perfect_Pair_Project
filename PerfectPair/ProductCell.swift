31905d129b8dfc97356d8be0fd6607c6138ac441
haya <haya.Adblrhman.med.s@tuwaiq.edu.sa>
2021-11-23 14:54:11 +0300

    at first created ViewController   with  Delegate ,DataSource collection view cell add for cell label image view connect them with the cell

---
+++ PerfectPair/ProductCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProductCell.swift
+//  PerfectPair
+//
+//  Created by Haya on 17/04/1443 AH.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var buTitel: UILabel!
+    @IBOutlet weak var buImage: UIImageView!
+    @IBOutlet weak var lblStatus: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
